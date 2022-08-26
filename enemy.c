cca988ab6375fcd1fc0b82387a5e689db47d0756
ygoksu <goxuyusuf@gmail.com>
2022-08-26 16:39:42 +0300

    asd

--- enemy.c
+++ enemy.c
@@ -2,7 +2,23 @@
 
 void    puts_enemy(t_vars   *ysf)
 {
-
+	static	int	fl = 0;//baska yerde tanimla
+	if (ysf->map[ysf->chr->e_y][ysf->chr->e_x + 1] != '1' && fl == 0)
+	{	
+	mlx_put_image_to_window(ysf->mlx, ysf->win,
+			ysf->enemy1, (ysf->chr->e_x + 1) * 64,ysf->chr->e_y * 64);
+	ysf->chr->e_x++;
+	if (ysf->map[ysf->chr->e_y][ysf->chr->e_x + 1] == '1')
+		fl = 1;
+	}
+	else if (ysf->map[ysf->chr->e_y][ysf->chr->e_x - 1] != '1' && fl == 1)
+	{
+		mlx_put_image_to_window(ysf->mlx, ysf->win,
+				ysf->enemy2, (ysf->chr->e_x - 1) * 64, ysf->chr->e_y * 64);
+		ysf->chr->e_y--;
+		if (ysf->map[ysf->chr->e_y][ysf->chr->e_x - 1] == '1')
+			fl = 0;
+	}
 }
 
 void    find_enemy(t_vars   *ysf)
@@ -17,10 +33,19 @@ void    find_enemy(t_vars   *ysf)
 		{
 			if (ysf->map[i][j] == 'D')
 			{
+				ysf->harida->enemy_cnt++;
 				ysf->chr->e_x = j;
-				
+				ysf->chr->e_y = i;
 			}
 				
 		}
 	}
+	if (ysf->harida->enemy_cnt < 1)
+	{
+		ft_printf("Dusman sayisi hatasi!");
+		exit (0);
+	}
 }
+
+if (ysf->chr->o_x == ysf->chr->e_x && ysf->chr->o_y == ysf->chr->e_y)
+	close_frame(ysf);
